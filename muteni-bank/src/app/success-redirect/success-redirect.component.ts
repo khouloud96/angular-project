d910a6081c725363f5ea4a6edc043dc99aa3a14a
Khouloud Ben Salem <khouloud.bensalem@ensi-uma.tn>
2024-11-26 09:38:07 +0100

    fix redirect from /form-sent-success to /profile page

--- muteni-bank/src/app/success-redirect/success-redirect.component.ts
+++ muteni-bank/src/app/success-redirect/success-redirect.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success-redirect',
   templateUrl: './success-redirect.component.html',
-  styleUrl: './success-redirect.component.css'
+  styleUrl: './success-redirect.component.css',
 })
 export class SuccessRedirectComponent {
+  constructor(private router: Router) {}
 
+  redirect() {
+    this.router.navigate(['/']);
+  }
 }
